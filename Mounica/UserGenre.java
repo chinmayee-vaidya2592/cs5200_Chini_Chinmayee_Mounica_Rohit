42b05c408efb01c806058f00e4dd8b21e33c74e3
mounicagadi <g.mounica93@gmail.com>
2016-11-28 22:51:13 -0500

    Create UserGenre.java

---
+++ Mounica/UserGenre.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbproject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author mounica
+ */
+public class UserGenre {
+    
+    private GenreType type;
+    
+    public GenreType gettype(){
+        return type;
+    }
+    
+    public void settype(GenreType t){
+        this.type = t;
+    }
+    public UserGenre(){
+        
+    }
+    
+    public UserGenre getug(GenreType t){
+        UserGenre ug = new UserGenre();
+        ug.settype(t);
+        return ug;
+    }
+}
