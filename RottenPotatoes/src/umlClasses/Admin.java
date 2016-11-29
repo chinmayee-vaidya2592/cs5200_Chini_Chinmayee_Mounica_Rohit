d9ab05a84f39345be8bd2b4ef4efbfd7f352c4de
csinha <csinha@basistech.com>
2016-11-28 23:47:22 -0500

    Changes by Chinmayee

---
+++ RottenPotatoes/src/umlClasses/Admin.java
@@ -0,0 +1,43 @@
+package umlClasses;
+
+
+public class Admin extends RegisteredUser{
+
+	/**
+	 * @param user creates a new user according to the registered user object passed
+	 */
+	public void createUser(RegisteredUser user){
+		
+	}
+	
+	/**
+	 * @param user: updates values of a particular user
+	 */
+	public void updateUser(RegisteredUser user){
+		
+	}
+	
+	/**
+	 * 
+	 * @param id Deletes a user for the particular id
+	 */
+	public void deleteUser(int id){
+		
+	}
+	
+	/**
+	 * 
+	 * @param id: deletes one comment according to the comment id passed
+	 */
+	public void deleteOneComment(int id){
+		
+	}
+	
+	/**
+	 * 
+	 * @param id: deletes all comments for a particular user id
+	 */
+	public void deleteAllComments(int id){
+		
+	}
+}
---
+++ RottenPotatoes/src/umlClasses/Artist.java
@@ -0,0 +1,16 @@
+package umlClasses;
+
+public class Artist extends RegisteredUser{
+
+	public ArtistType type;
+
+	public ArtistType getType() {
+		return type;
+	}
+
+	public void setType(ArtistType type) {
+		this.type = type;
+	}
+		
+		
+}
